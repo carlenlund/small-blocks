76011f14471dcc4947c82b8370a497d480279039
Carl Enlund <carl.enlund@gmail.com>
2020-12-01 20:39:02 +0100

    Increase render distance

--- infinite-demo/src/index.js
+++ infinite-demo/src/index.js
@@ -29,6 +29,8 @@ var nodeWidth = 4;
 var nodeHeight = 400;
 var layerSize = 151;
 var playerSize = 16;
+var playerSpeed = 8;
+var numLayers = 3;
 
 var canvas = document.querySelector('#canvas');
 canvas.width = 800;
@@ -40,22 +42,21 @@ var noise2d = openSimplexNoise.makeNoise2D(Date.now());
 
 var player = {
   size: playerSize,
+  speed: playerSpeed,
   layer: null,
   zoom: 0,
   x: 0,
 };
 window.player = player;  // For debugging
 
-var layers = [];
-
 // Initialize world.
-var layer = new Layer(layerSize);
-for (var i = 0; i < layerSize; ++i) {
-  layer.nodes[i] = new Node('none');
+var layers = new Array(numLayers);
+for (var i = 0; i < layers.length; ++i) {
+  layers[i] = new Layer(layerSize);
 }
-layers.push(layer);
-player.layer = layer;
 loadWorld();
+
+player.layer = layers[Math.floor(layers.length / 2)];
 
 render();
 
@@ -85,10 +86,10 @@ function getChunkPosition(x) {
 }
 
 function moveLeft() {
-  --player.x;
+  player.x -= player.speed;
   player.x = Math.ceil(player.x);
 
-  var x = utils.modulo(player.x + 1 + layerSize / 2, layerSize) - layerSize / 2 - 1;
+  var x = utils.modulo(player.x + player.speed + layerSize / 2, layerSize) - layerSize / 2 - player.speed;
   if (x < -Math.floor(layerSize / 2)) {
     loadWorld();
   }
@@ -97,10 +98,10 @@ function moveLeft() {
 }
 
 function moveRight() {
-  ++player.x;
+  player.x += player.speed;
   player.x = Math.floor(player.x);
 
-  var x = utils.modulo(player.x - 1 + layerSize / 2, layerSize) - layerSize / 2 + 1;
+  var x = utils.modulo(player.x - player.speed + layerSize / 2, layerSize) - layerSize / 2 + player.speed;
   if (x > Math.floor(layerSize / 2)) {
     loadWorld();
   }
@@ -117,11 +118,12 @@ function zoomOut() {
     for (var i = 0; i < layerSize; ++i) {
       layer.nodes[i] = new Node('none');
     }
-    generateWorld(layer, player.zoom, player.x);
+    // TODO: Store layer array in separate array? For persistence.
+    // And make sure not to regenerate already generated terrain.
+    loadWorld();
     player.layer.parent = layer;
     layer.child = player.layer;
     layer.updateFromChildLayer(player.layer);
-    layers.unshift(layer);
   }
   if (player.layer.dirtyNegative) {
     var parent = player.layer.parent.parent;
@@ -149,8 +151,6 @@ function zoomIn() {
     player.layer.child = layer;
     layer.parent = player.layer;
     layer.updateFromParentLayer(player.layer);
-
-    layers.push(layer);
   }
   if (player.layer.dirtyPositive) {
     var parent = player.layer;
@@ -185,11 +185,17 @@ function breakBlock() {
 }
 
 function loadWorld() {
-  var layer = player.layer;
-  for (var i = 0; i < layerSize; ++i) {
-    layer.nodes[i] = new Node('none');
+  for (var i = 0; i < layers.length; ++i) {
+    for (var j = 0; j < layerSize; ++j) {
+      layers[i].nodes[j] = new Node('none');
+    }
   }
-  generateWorld(layer, player.zoom, player.x);
+  var centerIndex = Math.floor(layers.length / 2);
+  generateWorld(layers[centerIndex], player.zoom, player.x);
+  for (var i = 1; i <= centerIndex; ++i) {
+    generateWorld(layers[centerIndex - i], player.zoom, player.x - layerSize);
+    generateWorld(layers[centerIndex + i], player.zoom, player.x + layerSize);
+  }
 }
 
 function generateWorld(layer, zoom, x) {
@@ -204,13 +210,12 @@ function generateWorld(layer, zoom, x) {
   var chunkX = getChunkPosition(x);
 
   for (var i = 0; i < layer.nodes.length; ++i) {
-    var noise = 0;
-    noise += 0.8 * (noise2d(0.05 * (i - Math.floor(layerSize / 2) + chunkX * layerSize) * Math.pow(2, -zoom), 0) + 1) / 2;
-    noise += 0.4 * (noise2d(0.5 * (i - Math.floor(layerSize / 2) + chunkX * layerSize) * Math.pow(2, -zoom), 1) + 1) / 2;
-    noise -= 0.1;
-    noise = Math.min(0.9999999999, Math.max(0, noise));
-    var colorName = colorNames[Math.floor(noise * colorNames.length)];
-    layer.nodes[i].value = colorName;
+    var noiseR = (noise2d(0.01 * (i - Math.floor(layerSize / 2) + chunkX * layerSize) * Math.pow(2, -zoom), 0) + 1) / 2;
+    var noiseG = (noise2d(0.01 * (i - Math.floor(layerSize / 2) + chunkX * layerSize) * Math.pow(2, -zoom), 100) + 1) / 2;
+    var noiseB = (noise2d(0.01 * (i - Math.floor(layerSize / 2) + chunkX * layerSize) * Math.pow(2, -zoom), 200) + 1) / 2;
+    // var colorName = colorNames[Math.floor(noise * colorNames.length)];
+    // layer.nodes[i].value = colorName;
+    layer.nodes[i].value = 'rgb(' + Math.round(noiseR * 255) + ',' + Math.round(noiseG * 255) + ',' + Math.round(noiseB * 255) + ')';
   }
   layer.dirtyNegative = true;
   layer.dirtyPositive = true;
@@ -223,12 +228,15 @@ function render() {
   var canvasCenterX = canvas.width / 2;
   var canvasCenterY = canvas.height / 2;
   var layerOffset = nodeHeight * 3;
-  var centerIndex = Math.floor(layers.length / 2);
 
-  var layer = player.layer;
+  var centerIndex = Math.floor(layers.length / 2);
   var layerY = canvasCenterY;
   var layerScale = 1;
-  renderLayer(layer, layerY, layerScale, player.x);
+  renderLayer(layers[centerIndex], layerY, layerScale, player.x, 0);
+  for (var i = 1; i <= centerIndex; ++i) {
+    renderLayer(layers[centerIndex - i], layerY, layerScale, player.x, -i);
+    renderLayer(layers[centerIndex + i], layerY, layerScale, player.x, i);
+  }
 
   var playerX = canvasCenterX;
   var playerY = layerY - nodeHeight / 2;
@@ -236,7 +244,7 @@ function render() {
   renderPlayer(playerX, playerY, playerSize);
 }
 
-function renderLayer(layer, y, scale, playerX) {
+function renderLayer(layer, y, scale, playerX, offset) {
   playerX = utils.modulo(playerX + layerSize / 2, layerSize) - layerSize / 2;
   var canvasCenterX = canvas.width / 2;
   var centerIndex = Math.floor(layer.nodes.length / 2);
@@ -247,14 +255,16 @@ function renderLayer(layer, y, scale, playerX) {
     var x = canvasCenterX
         - renderDistance * nodeWidth * scale
         + i * nodeWidth * scale
-        - playerX * nodeWidth;
-    var value = values[node.value];
-    var color = value.color;
-    if (color === null) {
-      ctx.fillStyle = '#000';
-    } else {
-      ctx.fillStyle = color;
-    }
+        - playerX * nodeWidth
+        + offset * layerSize * nodeWidth;
+    // var value = values[node.value];
+    // var color = value.color;
+    // if (color === null) {
+    //   ctx.fillStyle = '#000';
+    // } else {
+    //   ctx.fillStyle = color;
+    // }
+    ctx.fillStyle = node.value;
     ctx.fillRect(Math.floor(x - nodeWidth / 2),
                  Math.floor(y - nodeHeight / 2),
                  Math.floor(nodeWidth * scale),
